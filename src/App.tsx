22210abcdbe59d164ba55bbff55444038995b14c
Erfan-Karimipour <programming1402@gmail.com>
2023-05-02 21:07:34 +0430

    Task 2 created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css'
-import { Container } from './components/Container'
 
 function App() {
 
   return (
-    <div className='bg-[#fbcfe8] w-screen h-screen flex justify-center items-center'>
-      <Container />
+    <div>
+      Hello there
     </div>
   )
 }
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <button type='submit' className='w-full bg-purple-600 text-white py-2 text-lg text-center my-7 rounded-lg'>
-      Sign up
-    </button>
-  )
-}
